1f019775bf7fa54ac8396200715328d2047d7e04
Dan Codae <drcoda@gmail.com>
2018-08-17 21:04:37 -0400

    initial commit. added player

--- keyboarder.js
+++ keyboarder.js
@@ -1,3 +1,54 @@
+
+
+class Game {
+  constructor() {
+    this.canvas = document.getElementById('screen')
+    this.context = this.canvas.getContext('2d')
+    this.size = {x: this.canvas.width, y: this.canvas.height}
+
+    this.player = new Player(this,{
+      
+    })    
+
+
+    this.ticks = 0
+
+    let tick = () => {
+        this.ticks++
+        this.update()
+        this.draw()
+        if (!this.gameOver) {
+            window.requestAnimationFrame(tick)
+        }
+    }
+    this.tick = tick
+  }
+  update() {
+    console.log('updating')
+  }
+  draw(){
+    console.log('drawing')
+    this.context.fillRect(this.size.x/2-15, 450, 15, 15)
+    this.player.draw
+  }
+  start(){
+    this.tick()
+  }
+}
+
+
+
+
+class Player {
+  constructor (){
+
+  }
+}
+
+
+
+
+
 class Keyboarder {
   constructor () {
     this.keyState = {}
@@ -25,3 +76,6 @@ class Keyboarder {
 }
 
 Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40, S: 83 }
+
+let game = new Game()
+game.start()
