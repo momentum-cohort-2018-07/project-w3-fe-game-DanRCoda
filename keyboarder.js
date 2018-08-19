4dcdd50e158561bd31d8bcd431643ecf3751e1d6
Dan Codae <drcoda@gmail.com>
2018-08-19 15:46:11 -0400

    moved game script to separate js file

--- keyboarder.js
+++ keyboarder.js
@@ -1,86 +1,6 @@
-const TICKS_PER_MOVE = 10
-
-
-
-class Game {
-  constructor() {
-    this.canvas = document.getElementById('screen')
-    this.context = this.canvas.getContext('2d')
-    this.size = {x: this.canvas.width, y: this.canvas.height}
-
-    this.player = new Player(this,{
-    })    
-
-
-    this.ticks = 0
-
-    let tick = () => {
-        this.update()
-        this.draw()
-        if (!this.gameOver) {
-            window.requestAnimationFrame(tick)
-        }
-    }
-    this.tick = tick
-  }
-
-  draw(){
-    // console.log('drawing')
-    this.context.fillRect(this.size.x/2-15, 450, 15, 15)
-    this.player.draw
-  }
-  update() {
-    this.player.update()
-  }
-
-  start(){
-    this.tick()
-  }
-}
-
-
-
-
-class Player {
-  constructor(game) {
-    this.center = {
-        x:  this.size/ 2 - 10,
-        y: 450
-    }
-    this.playerSize = {
-        x: 20,
-        y: 20
-    }
-    
-    this.keyboarder = new Keyboarder()
-    this.game = game
-  }
-
-  draw () {
-    this.context.fillRect(this.center.x, this.center.y, this.playerSize.x,
-      this.playerSize.y)
-  }
-
-update() {
-  if
-  (this.keyboarder.isDown(Keyboarder.KEYS.LEFT)) {
-    this.center.x -= 2
-    if (this.center.x <=0) this.center.x = 0
-  }
- if (this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)) {
-            this.center.x += 2
-            if (this.center.x >= 480) this.center.x = 480
-        }
-  }
-
-
-
-}
-
-
 
 class Keyboarder {
-  constructor () {
+  constructor() {
     this.keyState = {}
 
     window.addEventListener('keydown', function (e) {
@@ -92,11 +12,11 @@ class Keyboarder {
     }.bind(this))
   }
 
-  isDown (keyCode) {
+  isDown(keyCode) {
     return this.keyState[keyCode] === true
   }
 
-  on (keyCode, callback) {
+  on(keyCode, callback) {
     window.addEventListener('keydown', function (e) {
       if (e.keyCode === keyCode) {
         callback()
@@ -105,7 +25,11 @@ class Keyboarder {
   }
 }
 
-Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40, S: 83 }
+Keyboarder.KEYS = {
+  LEFT: 37,
+  RIGHT: 39,
+  UP: 38,
+  DOWN: 40,
+  S: 83
+}
 
-let game = new Game()
-game.start()
